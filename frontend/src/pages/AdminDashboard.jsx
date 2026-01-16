8f080414990afd137fd610a553f334776bcdf7ff
Mateus Souza <mateusouza14@gmail.com>
2026-01-16 15:07:02 -0300

    fix: adicionar admin-master

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -253,7 +253,7 @@ export default function AdminDashboard() {
       <h1>ADMIN DASHBOARD CARREGOU</h1>
     </div>
   );
-}
+
 
 const [usersList, setUsersList] = useState([]);
 const [reservationsList, setReservationsList] = useState([]);
